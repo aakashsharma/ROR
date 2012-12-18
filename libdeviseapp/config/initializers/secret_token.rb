437fce155a34bde5dbab3e8e4e3e54fd7cb34107
aakashsharma <akash.sharma09@gmail.com>
2012-12-18 11:19:27 +0530

    devise app push

---
+++ libdeviseapp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Libdeviseapp::Application.config.secret_token = 'b89bea08af95f1e71b7e4a5d7e2c244de4e5b7bf1ff3f9a0fae475645e079682512767c42b43c93be2dcbc4d06e7f9b86ece7d82deb29b8642091b0b29d7d645'
---
+++ libdeviseapp/config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Libdeviseapp::Application.config.session_store :cookie_store, key: '_libdeviseapp_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Libdeviseapp::Application.config.session_store :active_record_store
