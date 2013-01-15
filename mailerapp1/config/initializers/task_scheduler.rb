0e20f858285d817ba6e8f88c56bdeeb26b25c90b
aakashsharma <akash.sharma09@gmail.com>
2013-01-15 12:13:30 +0530

    scheduler time set for every day at 10 am

--- mailerapp1/config/initializers/task_scheduler.rb
+++ mailerapp1/config/initializers/task_scheduler.rb
@@ -5,7 +5,7 @@ scheduler = Rufus::Scheduler.start_new
 
 # Send the digest every day at noon
  
-scheduler.every ''0 10 * * 1-7'' do
+scheduler.cron '0 10 * * 1-7' do
 	# every day at 10 Am
 	
 	system 'bundle exec rake notify:birthday_users'
